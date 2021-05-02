fb01aa844d8375b4e0da934306eb20e5457f8964
SilentWindow <68240643+SilentWindow@users.noreply.github.com>
2021-05-01 21:11:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace number_guessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do {
+                Console.Write("Guess a number inbetween 0 and 100:  ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum) {
+                    Console.WriteLine("Too high: Guess Lower...");
+                } else if (i < winNum) {
+                    Console.WriteLine("Too Low: Guess Higher...");
+                } else if (i == winNum) {
+                    Console.WriteLine("YOU WIN");
+                    win = true;
+                }
+
+                Console.WriteLine();
+            } while (win == false);
+        }
+    }
+}
